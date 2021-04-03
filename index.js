73b7665bd73adceaa617297c5fff1512a410632d
tranxuanthinh <thinhtxph12298@fpt.edu.vn>
2021-04-03 13:50:00 +0700

    song

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express')
+var expressHDS = require('express-handlebars')
+var app = express()
+var path = require('path')
+
+
+app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.static(path.join(__dirname, '/assets')));
+app.engine("handlebars",expressHDS({layoutsDir: __dirname + '/views/layouts', defaultLayout: 'main'}))
+app.set('view engine','handlebars')
+app.get('/',function (request, response){
+    response.render('home')
+});
+app.post('/login',function (request,response){
+
+});
+app.listen(process.env.PORT || '4000')
